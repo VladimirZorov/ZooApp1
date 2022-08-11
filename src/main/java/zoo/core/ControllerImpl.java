85ed80b1089dc8995b664d9b8f5a1df4989ceb80
Vladimir Zorov <vl.zorov@gmail.com>
2022-08-11 20:46:45 +0300

    Add file

--- src/main/java/zoo/core/ControllerImpl.java
+++ src/main/java/zoo/core/ControllerImpl.java
@@ -1,20 +1,82 @@
 package zoo.core;
 
+import zoo.entities.areas.Area;
+import zoo.entities.areas.LandArea;
+import zoo.entities.areas.WaterArea;
+import zoo.entities.foods.Food;
+import zoo.entities.foods.Meat;
+import zoo.entities.foods.Vegetable;
+import zoo.repositories.FoodRepository;
+import zoo.repositories.FoodRepositoryImpl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static zoo.common.ConstantMessages.*;
+import static zoo.common.ExceptionMessages.*;
+
 public class ControllerImpl implements Controller {
+
+    private FoodRepository foodRepository;
+    private Map<String, Area> areas;
+
+    public ControllerImpl() {
+        this.foodRepository = new FoodRepositoryImpl();
+        this.areas = new LinkedHashMap<>();
+    }
 
     @Override
     public String addArea(String areaType, String areaName) {
-        return null;
+        Area area;
+
+        if (areaType.equals("WaterArea")) {
+            area = new WaterArea(areaName);
+        } else if (areaType.equals("LandArea")) {
+            area = new LandArea(areaName);
+        } else {
+            throw new NullPointerException(INVALID_AREA_TYPE);
+        }
+
+        areas.put(areaName,area);
+
+        return String.format(SUCCESSFULLY_ADDED_AREA_TYPE, areaType);
     }
 
     @Override
     public String buyFood(String foodType) {
-        return null;
+
+        Food food;
+
+        if (foodType.equals("Vegetable")) {
+            food = new Vegetable();
+        }else if(foodType.equals("Meat")) {
+            food = new Meat();
+        }else {
+            throw new IllegalArgumentException(INVALID_FOOD_TYPE);
+        }
+
+        foodRepository.add(food);
+
+        return String.format(SUCCESSFULLY_ADDED_FOOD_TYPE, foodType);
     }
 
     @Override
     public String foodForArea(String areaName, String foodType) {
-        return null;
+        Food food = foodRepository.findByType(foodType);
+
+        if (food == null) {
+            throw new IllegalArgumentException(NO_FOOD_FOUND, foodType);
+        }
+
+        Area area = areas.get(areaName);
+        area.addFood(food);
+
+        foodRepository.remove(food);
+
+        return String.format(SUCCESSFULLY_ADDED_FOOD_IN_AREA, foodType, areaName);
+
     }
 
     @Override
--- src/main/java/zoo/entities/foods/Meat.java
+++ src/main/java/zoo/entities/foods/Meat.java
@@ -4,7 +4,7 @@ public class Meat extends BaseFood{
     public static final int CALORIES = 70;
     public static final double PRICE = 10.00;
 
-    protected Meat() {
+    public Meat() {
         super(CALORIES, PRICE);
     }
 }
